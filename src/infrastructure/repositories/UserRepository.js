4e75dfe6cf1bd262bb4ba0ff4aa95141f608b008
Fakhrul <rachmanfakhrul@gmail.com>
2026-09-15 13:15:48 +0700

    chore: remove dead code files (old Sidebar, unused DashboardView, and redundant UserRepository alias)

--- src/infrastructure/repositories/UserRepository.js
+++
@@ -1,8 +0,0 @@
-import { SupabaseUserRepository } from './SupabaseUserRepository.js';
-
-/**
- * UserRepository
- * Alias & Singleton wrapper untuk SupabaseUserRepository demi backwards-compatibility.
- */
-export const UserRepository = SupabaseUserRepository;
-export { SupabaseUserRepository };
